af7d37d0201f829cfbbf99a93c39d71d2e85a5a1
Juan Manuel Pérez <junperz163@gmail.com>
2025-01-15 16:28:12 +0100

    añadir busqueda en home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,12 +7,14 @@ import { FavoriteAddComponent } from './pages/favorite-add/favorite-add.componen
 import { HomeComponent } from './pages/home/home.component';
 import { LoginGoogleComponent } from './pages/login-google/login-google.component';
 import { AuthGuard } from './guards/auth.guard';
+import { PartyListComponent } from './pages/party-list/party-list.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'login', component: LoginGoogleComponent},
-    { path: 'favs', component: FavoriteListComponent, canActivate: [AuthGuard]},
+    { path: 'favs', component: FavoriteListComponent},
     { path: 'favs/add', component: FavoriteAddComponent, canActivate: [AuthGuard]},
     { path: 'favs/edit/:id', component: FavoriteEditComponent, canActivate: [AuthGuard]},
+    { path: 'parties', component: PartyListComponent},
     { path: '**', component: ErrorPageComponent }
 ];
